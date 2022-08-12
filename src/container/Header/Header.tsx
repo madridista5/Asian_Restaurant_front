835a0beebeaca7bf13963cbe2faf99158931b8b0
Tymoteusz Lossy <tymek.lossy@gmail.com>
2022-08-12 17:50:44 +0200

    Component AboutUs for mobile was finished.

--- src/container/Header/Header.tsx
+++ src/container/Header/Header.tsx
@@ -7,7 +7,7 @@ export const Header = () => (
   <div className="app__header app__wrapper section__padding" id="home">
       <div className="app__wrapper_info">
           <SubHeading title="Spróbuj nowego smaku"/>
-          <h1 className="app__header-h1">Restauracja Asian Food</h1>
+          <h1 className="app__header-h1">Asian Food</h1>
           <p className="p__opensans" style={{margin: '2rem 0'}}>Asian Food to podróż do krainy pełnej aromatycznego jedzenia, zapachów i niecodziennych smaków.</p>
           <button type="button" className="custom__button"><a href="#menu">Zobacz menu</a></button>
       </div>
