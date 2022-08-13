edf8902f3586f18e8a7657a2a10a7d1451655d7a
Tymoteusz Lossy <tymek.lossy@gmail.com>
2022-08-13 15:33:31 +0200

    Component Intro was finished.

--- src/container/Intro/Intro.tsx
+++ src/container/Intro/Intro.tsx
@@ -1,9 +1,43 @@
-import React from 'react';
+import React, {useRef, useState} from 'react';
+import {BsFillPlayFill, BsPauseFill} from 'react-icons/bs';
 
 import './Intro.css';
 
-export const Intro = () => (
-  <div>
-    Intro
-  </div>
-);
+export const Intro = () => {
+    const [playVideo, setPlayVideo] = useState<boolean>(false);
+    let vidRef = useRef<HTMLVideoElement | null>(null);
+
+    const handleVideo = () => {
+        setPlayVideo(prevState => !prevState);
+    };
+
+    if (playVideo) {
+        vidRef.current?.play();
+    } else {
+        vidRef.current?.pause();
+    }
+
+    return (
+        <div className="app__video">
+            <video
+                src={require('../../assets/meal.mp4')}
+                ref={vidRef}
+                loop
+                controls={false}
+                muted
+                className="video-js"
+            />
+            <div className="app__video-overlay flex__center">
+                <div
+                    className="app__video-overlay_circle flex__center"
+                    onClick={handleVideo}
+                >
+                    {playVideo
+                        ? <BsPauseFill color="#fff" fontSize={30}/>
+                        : <BsFillPlayFill color="#fff" fontSize={30}/>
+                    }
+                </div>
+            </div>
+        </div>
+    );
+};
