2ce6508418564564e7818e54d0fa0a999f7660c1
Tymoteusz Lossy <tymek.lossy@gmail.com>
2022-08-20 12:49:10 +0200

    Component AddToOrderInfo was finished.

--- src/components-routes/AddToOrderInfo/AddToOrderInfo.tsx
+++ src/components-routes/AddToOrderInfo/AddToOrderInfo.tsx
@@ -1,12 +1,26 @@
 import React, {useContext} from "react";
 import {NavbarRoutes} from "../NavbarRoutes/NavbarRoutes";
 import {SelectedDishContext} from "../../contexts/selectedDish.context";
+import {SubHeading} from "../../components";
+import {Link} from "react-router-dom";
 
 export const AddToOrderInfo = () => {
     const selectedDish = useContext(SelectedDishContext);
 
     return <>
         <NavbarRoutes/>
-        <h1>Wybrane danie: {selectedDish.name}, cena: {selectedDish.price}</h1>
+
+        <div className="app__header app__wrapper section__padding">
+            <div className="app__wrapper_info">
+                <SubHeading title="Potwierdzenie"/>
+                <h1 className="app__header-h1">Asian Food</h1>
+                <p className="p__opensans" style={{margin: '2rem 0'}}>Danie "{selectedDish.name}" zostało dodane do Twojego zamówienia.</p>
+                <button type="button" className="custom__button"><Link to="/menu">Powrót do menu</Link></button>
+            </div>
+
+            <div className="app__wrapper_img">
+                <img src={require('../../assets/welcome.jpg')} alt="header img"/>
+            </div>
+        </div>
     </>
 }
