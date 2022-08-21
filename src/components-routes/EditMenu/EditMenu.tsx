b97f59fcd6da14be1bea56557ecdd144900ac995
Tymoteusz Lossy <tymek.lossy@gmail.com>
2022-08-21 15:11:35 +0200

    Component EditMenu was created and finished.

--- src/components-routes/EditMenu/EditMenu.tsx
+++ src/components-routes/EditMenu/EditMenu.tsx
@@ -1,12 +1,14 @@
-import React, {useEffect, useState} from "react";
+import React, {useContext, useEffect, useState} from "react";
 import {NavbarRoutes} from "../NavbarRoutes/NavbarRoutes";
 import {axiosFunction} from "../../utils/axios-function";
 import { DishResponse } from "types";
 import {MenuItem, SubHeading} from "../../components";
 import {Link} from "react-router-dom";
+import {SelectedCategoryContext} from "../../contexts/selectedCategory.context";
 
 export const EditMenu = () => {
     const [dishes, setDishes] = useState<DishResponse[]>([]);
+    const {setCategory} = useContext(SelectedCategoryContext);
 
     useEffect(() => {
         (async () => {
@@ -14,6 +16,10 @@ export const EditMenu = () => {
             setDishes(data);
         })();
     }, []);
+
+    const handleCategory = (selectedCategory: string) => {
+      setCategory(selectedCategory);
+    };
 
     return (
         <>
@@ -28,7 +34,7 @@ export const EditMenu = () => {
                 <div className="app__specialMenu-menu">
                     <div className="app__specialMenu-menu_pasta flex__center" style={{padding: '0 2rem 0 2rem'}}>
                         <p className="app__specialMenu-menu_heading">Makarony i ryż z WOK</p>
-                        <button type="button" className="custom__button" style={{margin: '1rem 0 1rem 0'}}>
+                        <button type="button" className="custom__button" style={{margin: '1rem 0 1rem 0'}} onClick={() => handleCategory('Makarony i ryż z WOK')}>
                             <Link to="/addDishToMenu">Dodaj danie do tej kategorii</Link>
                         </button>
                         <div className="app__specialMenu_menu_items">
@@ -43,7 +49,7 @@ export const EditMenu = () => {
 
                     <div className="app__specialMenu-menu_seafood flex__center" style={{padding: '0 2rem 0 2rem'}}>
                         <p className="app__specialMenu-menu_heading">Owoce morza</p>
-                        <button type="button" className="custom__button" style={{margin: '1rem 0 1rem 0'}}>
+                        <button type="button" className="custom__button" style={{margin: '1rem 0 1rem 0'}} onClick={() => handleCategory('Owoce morza')}>
                             <Link to="/addDishToMenu">Dodaj danie do tej kategorii</Link>
                         </button>
                         <div className="app__specialMenu_menu_items">
@@ -60,7 +66,7 @@ export const EditMenu = () => {
                 <div className="app__specialMenu-menu">
                     <div className="app__specialMenu-menu_pasta flex__center" style={{padding: '0 2rem 0 2rem'}}>
                         <p className="app__specialMenu-menu_heading">Zupy</p>
-                        <button type="button" className="custom__button" style={{margin: '1rem 0 1rem 0'}}>
+                        <button type="button" className="custom__button" style={{margin: '1rem 0 1rem 0'}} onClick={() => handleCategory('Zupy')}>
                             <Link to="/addDishToMenu">Dodaj danie do tej kategorii</Link>
                         </button>
                         <div className="app__specialMenu_menu_items">
@@ -75,7 +81,7 @@ export const EditMenu = () => {
 
                     <div className="app__specialMenu-menu_seafood flex__center" style={{padding: '0 2rem 0 2rem'}}>
                         <p className="app__specialMenu-menu_heading">Desery</p>
-                        <button type="button" className="custom__button" style={{margin: '1rem 0 1rem 0'}}>
+                        <button type="button" className="custom__button" style={{margin: '1rem 0 1rem 0'}} onClick={() => handleCategory('Desery')}>
                             <Link to="/addDishToMenu">Dodaj danie do tej kategorii</Link>
                         </button>
                         <div className="app__specialMenu_menu_items">
