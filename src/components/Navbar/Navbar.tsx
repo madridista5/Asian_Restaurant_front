c372fa268a089b7d7ab95c5cdd702e7b8b45f582
Tymoteusz Lossy <tymek.lossy@gmail.com>
2022-08-22 15:44:08 +0200

    Mobile view was fixed (two buttons were added to Navbar).

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -42,6 +42,10 @@ export const Navbar = () => {
                             <li className="p__opensans"><a href="#menu">Menu</a></li>
                             <li className="p__opensans"><a href="#awards">Nagrody</a></li>
                             <li className="p__opensans"><a href="#contact">Kontakt</a></li>
+                            <li className="p__opensans"><Link to="/login">Logowanie /
+                                Rejestracja</Link></li>
+                            <li className="p__opensans"><Link to="/order">Moje zamówienie</Link>
+                            </li>
                             {userRole && <li className="p__opensans"><Link to="/signOut">Wyloguj</Link></li>}
                             {userRole === 'ADMIN' && <li className="p__opensans"><Link to="/admin">Admin</Link></li>}
                         </ul>
